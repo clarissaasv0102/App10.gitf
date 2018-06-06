5d3237625b2dd27de808de1ddf0eda6b7e3b6675
clarissaasv0102 <38216408+clarissaasv0102@users.noreply.github.com>
2018-06-05 20:23:36 -0500

    Subida 3

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
-		$('#come').tap(function(){
+		$('#cua').tap(function(){
 			navigator.notification.vibrate(100);
 		}); 
 		$('#come').on('click',function(){
